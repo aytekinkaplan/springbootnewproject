ced0ca0b9b30ba9e209c4c854a83fddea8e8d1c4
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-08 00:10:51 +0300

    New Spring project commit

--- src/main/java/com/springworks/springbootnewproject/SpringbootnewprojectApplication.java
+++ src/main/java/com/springworks/springbootnewproject/SpringbootnewprojectApplication.java
@@ -1,13 +1,22 @@
 package com.springworks.springbootnewproject;
 
+import com.springworks.springbootnewproject.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SpringbootnewprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootnewprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootnewprojectApplication.class, args);
+    }
 
 }
