8fb33b2681d9c28bb7cd45b3b5e060ddfba05246
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-08 00:21:39 +0300

    New Spring project commit

--- src/main/java/com/springworks/springbootnewproject/student/StudentController.java
+++ src/main/java/com/springworks/springbootnewproject/student/StudentController.java
@@ -10,16 +10,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L, "Jill", "X9ZtX@example.com", LocalDate.of(2000, 8, 30), 23
-                )
-                , new Student(
-                        1L, "Jack", "X9ZtX@example.com", LocalDate.of(2000, 8, 30), 23
-                )
-        );
+        return studentService.getStudents();
     }
 
 }
---
+++ src/main/java/com/springworks/springbootnewproject/student/StudentService.java
@@ -0,0 +1,18 @@
+package com.springworks.springbootnewproject.student;
+
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(1L, "John", "Doe", LocalDate.of(2000, 1, 1), 20),
+                new Student(2L, "Jane", "Doe", LocalDate.of(2001, 1, 1), 21),
+                new Student(3L, "Jack", "Doe", LocalDate.of(2002, 1, 1), 22),
+                new Student(4L, "Jill", "Doe", LocalDate.of(2003, 1, 1), 23)
+        );
+    }
+}
