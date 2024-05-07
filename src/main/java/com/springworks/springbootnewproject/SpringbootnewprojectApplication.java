3162df87ebd88881b05723ed4b22bdd461665247
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-07 20:39:07 +0300

    Initial commit

---
+++ src/main/java/com/springworks/springbootnewproject/SpringbootnewprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springworks.springbootnewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootnewprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootnewprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springworks/springbootnewproject/SpringbootnewprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springworks.springbootnewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootnewprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
